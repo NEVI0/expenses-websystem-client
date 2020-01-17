c2c109379b7839db1e67a615011c16f06d97406c
Nevio <neviocostamagagnin@gmail.com>
2020-01-17 14:27:57 -0200

    Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  OpenApiUrl: "https://expenses-server-api.herokuapp.com/oapi",
+  BlockedApiUrl: "https://expenses-server-api.herokuapp.com/api"
 };
