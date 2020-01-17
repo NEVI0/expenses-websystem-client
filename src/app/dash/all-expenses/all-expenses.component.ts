75443d87f30fac457c47fc4b47f426cdd2d83d75
Nevio <neviocostamagagnin@gmail.com>
2020-01-17 14:27:18 -0200

    Adding the All Expenses Component

--- src/app/dash/all-expenses/all-expenses.component.ts
+++ src/app/dash/all-expenses/all-expenses.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Observable, Subject, empty } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
+import { Expense } from '../../interfaces/Expense';
+import { DashService } from '../dash.service';
 
 @Component({
-  selector: 'app-all-expenses',
-  templateUrl: './all-expenses.component.html',
-  styleUrls: ['./all-expenses.component.scss']
+    selector: 'app-all-expenses',
+    templateUrl: './all-expenses.component.html',
+    styleUrls: ['./all-expenses.component.scss']
 })
+
 export class AllExpensesComponent implements OnInit {
 
-  constructor() { }
+    expenses$: Observable<Expense[]>;
+	error$ = new Subject<boolean>();
+	displayedColumns: string[];
 
-  ngOnInit() {
-  }
+	constructor(
+		private dashService: DashService,
+		private dialog: MatDialog
+	) {}
+
+	ngOnInit() {
+		this.onRefresh();
+		this.displayedColumns = ['name', 'value', 'createdAt', 'actions'];
+	}
+
+	onRefresh() {
+		this.expenses$ = this.dashService.getExpenses().pipe(
+			catchError(err => {
+				this.error$.next(err);
+				return empty();
+			})
+		);
+	}
+
+	onShowAlert(_id: string) {
+		console.log("Mosta uma mensagem de alerta!");
+	}
 
 }
