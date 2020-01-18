5711086ef9b8a9dd94dd0681c05d364c33cb1f82
Nevio <neviocostamagagnin@gmail.com>
2020-01-18 17:07:30 -0200

    Adding the Auth Guard to block some routes in the app

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean> | Promise<boolean> | boolean {
+        const json = JSON.parse(this.authService.isAuthenticate());
+
+        if (json == undefined || json == null || json.token == undefined || json.token == null || json.token == '') {
+            this.router.navigate(['/auth']);
+            return false;
+        }
+
+        return true;
+
+    }
+
+}
