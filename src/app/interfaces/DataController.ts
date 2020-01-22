50b32da47eb30bbe73ff77d3f76706581be6c195
Nevio <neviocostamagagnin@gmail.com>
2020-01-22 11:36:04 -0200

    Fixing the interfaces

--- src/app/interfaces/DataController.ts
+++ src/app/interfaces/DataController.ts
@@ -1,9 +1,5 @@
 export interface DataController {
 	success: boolean;
-	userId: string;
-	userName: string;
-	userEmail: string;
-	userSalary: number;
 	numberOfExpenses: number;
 	sumOfValues: number;
 	avgOfValues: number;
--- src/app/interfaces/Expense.ts
+++ src/app/interfaces/Expense.ts
@@ -6,6 +6,5 @@ export interface Expense {
 	date: Date;
 	createdAt: Date;
 	userId: string;
-	userData: Array<any>;
 	tags: Array<string>;
 }
