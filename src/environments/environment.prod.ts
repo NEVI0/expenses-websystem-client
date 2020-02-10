bb11e5811e7714db45b54327a721e91b7e40c15e
Nevio <neviocostamagagnin@gmail.com>
2020-02-10 20:34:03 -0200

    Doing the Firebase Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   OpenApiUrl: "https://expenses-server-api.herokuapp.com/oapi",
-  BlockedApiUrl: "https://expenses-server-api.herokuapp.com/api"
+  BlockedApiUrl: "https://expenses-server-api.herokuapp.com/api",
+  AppUserData: "_application_user_data",
+  AppTheme: "_application_theme"
 };
